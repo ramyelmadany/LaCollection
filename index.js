5e74d6b468c1e7fcba47def292cd92d042e6ac60
ramyelmadany <ramyelmadany@mac.com>
2026-01-21 18:07:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CigarCollectionApp from './CigarCollectionApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CigarCollectionApp />
+  </React.StrictMode>
+);
